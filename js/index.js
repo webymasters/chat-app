c04fc6e5b0a4c57835f0767d4a648245d3e20806
Rocco Panacci <rocco@Roccos-MacBook-Pro.local>
2020-06-09 11:16:23 -0400

    The beginning of a chat app with socketio

--- js/index.js
+++ js/index.js
@@ -1,3 +1,16 @@
 'use strict'
 
-console.log('Hello world')
+const socket = io()
+
+// Send a message to say that I've connected
+socket.emit('newuser', {user: 'Grace Hopper'})
+
+// Event listener, waiting for an incoming "newuser"
+socket.on('newuser', (data) => console.log(`${data.user} has connected!`))
+
+
+// Listen for the 'submit' of a form
+// 	 event.preventDefault()  (prevent the form from leaving the page)
+//   Emit a message using "chatmsg"
+// Listen for "chatmsg"
+//   add a <li> with the chat msg to the <ol>
---
+++ server.js
@@ -0,0 +1,33 @@
+const express  = require('express')
+const app      = express()
+const port     = 3000
+const server   = app.listen(port, () => {console.log(`Server running at http://localhost:${port}`)})
+const io 			 = require('socket.io').listen(server)
+
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.use(express.static('.'))
+
+
+
+
+io.on('connection', function (socket) {
+	
+   // Listen for a "newuser" message
+   socket.on('newuser', (data) => {
+      // Transmit a message to everyone except the sender
+      socket.broadcast.emit('newuser', data)
+
+      // The same message, sent to all users - try it!
+      //io.emit('newuser', data)
+	 })
+	 
+	 // Listen for "chatmsg"
+	 //   io.emit to all user
+
+
+
+})
