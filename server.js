f081fe6dee339f774cad403745742d8cf1aa79b6
Manpreet Singh <56766829+webymasters@users.noreply.github.com>
2020-06-25 15:27:33 -0400

    Port changes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express  = require('express')
 const app      = express()
-const port     = 3000
+const port     = process.env.PORT || 3000;
 const server   = app.listen(port, () => {console.log(`Server running at http://localhost:${port}`)})
 const io 			 = require('socket.io').listen(server)
 
